3cb5f1679a59eada1d30bba2f26d2face36e9dcc
Brian Warmerdam <repobrain@gmail.com>
2018-05-30 13:16:23 -0700

    Add first rev template.sh

---
+++ template.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash -e
+#
+#-----------------------------------------------------------------#
+# BASH Script Template - no code examples (replace with name
+#-----------------------------------------------------------------#
+# Author: Brian Warmerdam repobrain@gmail.com repobrain.github.io
+# Date:
+# Purpose:
+# Version: 0.1
+#----------0.2
+#----------0.3
+# History:
+#-----------------------------------------------------------------#
+#
+# For troubleshooting purposes:
+#set -o errexit
+#set -o pipefail
+#set -o nounset
+#set -o xtrace
+#
+
+
+
+
+
+exit 0
